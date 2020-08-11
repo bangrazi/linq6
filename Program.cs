b2b5c0c114a48f68e927b5b607949351c4ff75ee
Anthony Bangrazi <bangrazi@comcast.net>
2020-08-11 18:35:18 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello {nameof(linq6)}!");
+            Console.WriteLine($"Calculates a sequence of pairs for every combination in the input.");
+
+            IEnumerable<char> input = new [] { 'a', 'b', 'c', 'd', 'e' };
+            IEnumerable<(char, char)> output = input
+                .SelectMany(c1 => input.Except(new [] { c1 }).Select(c2 => (c1, c2)))
+                .ToList();
+
+            Console.WriteLine($"input={input.Count()}, output={output.Count()}");
+            foreach((char x, char y) pair in output) {
+                Console.WriteLine($"{pair.x}, {pair.y}");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # linq6
 Linq for combination generation
+
+Generates a sequence that contains all combinations of the elements in the input.
+
+Example:
+```
+input: (a, b, c) => output: ((a, b), (a, c), (b, a), (b, c), (c, a), (c, b))
+```
